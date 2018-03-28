48ad5ed7516ae777f3dbc6bc504b8adc096eea1e
Hunter Haydel <hhaydel@gmail.com>
2018-03-27 20:21:07 -0400

    Move Character Data To Another File

--- src/data/GraphqlServer.js
+++ src/data/GraphqlServer.js
@@ -2,54 +2,17 @@ const { graphqlExpress } = require("apollo-server-express");
 const { makeExecutableSchema } = require("graphql-tools");
 const fs = require("fs");
 const path = require("path");
+const characters = require("./characters");
 
 const typeDefs = fs
   .readFileSync(path.join(__dirname, "typeDefs.graphql"))
   .toString();
 
-const fakeData = [
-  {
-    id: 1,
-    name: "Rosie",
-    class: "Monk"
-  },
-  {
-    id: 2,
-    name: "K'Thriss",
-    class: "Warlock"
-  },
-  {
-    id: 3,
-    name: "Walnut",
-    class: "Druid"
-  },
-  {
-    id: 4,
-    name: "Donaar",
-    class: "Paladin"
-  },
-  {
-    id: 5,
-    name: "Magnus",
-    class: "Fighter"
-  },
-  {
-    id: 6,
-    name: "Mearle",
-    class: "Cleric"
-  },
-  {
-    id: 7,
-    name: "Taako",
-    class: "Wizard"
-  }
-];
-
 export function create() {
   const resolvers = {
     Query: {
       characters: (_, { offset, limit }) =>
-        fakeData.slice(offset, offset + limit)
+        characters.slice(offset, offset + limit)
     }
   };
 
---
+++ src/data/characters.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Rosie",
+    class: "Monk"
+  },
+  {
+    id: 2,
+    name: "K'Thriss",
+    class: "Warlock"
+  },
+  {
+    id: 3,
+    name: "Walnut",
+    class: "Druid"
+  },
+  {
+    id: 4,
+    name: "Donaar",
+    class: "Paladin"
+  },
+  {
+    id: 5,
+    name: "Magnus",
+    class: "Fighter"
+  },
+  {
+    id: 6,
+    name: "Mearle",
+    class: "Cleric"
+  },
+  {
+    id: 7,
+    name: "Taako",
+    class: "Wizard"
+  }
+];
